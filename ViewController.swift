168b4c589f1fd13116a9b3b554d9fc1ca9a7f17f
Faiq Ali <faiqueali017@gmail.com>
2021-02-17 16:26:01 +0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmDB
+//
+//  Created by Faiq on 17/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
